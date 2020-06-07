f0e473a987d54fc7e13510b8facfb99a0fbabe66
opsaaaaa <sean@ferney.org>
2020-06-06 18:48:38 -0600

    stuff

--- Wall.gd
+++ Wall.gd
@@ -1,7 +1,6 @@
 tool
 extends Line2D
 
-
 func set_points(new_points: PoolVector2Array)->void:
 	points = new_points
 	$StaticBody2D/CollisionPolygon2D.polygon = stroke_to_polygon()
@@ -11,9 +10,9 @@ func stroke_to_polygon():
 	
 	for i in range(points.size()):
 		new_polygon.append(get_offset_point(i, width / 2))
-		
+
 	new_polygon.invert()
-	
+
 	for i in range(points.size()):
 		new_polygon.append(get_offset_point(i, -width / 2))
 		
@@ -30,7 +29,7 @@ func get_relative_point(i, space):
 	if not i + space >= points.size() && i + space >= 0:
 		return points[i + space]	
 	return points[i]
-	
+
 
 func get_offset(last_point: Vector2, next_point: Vector2, offset: int)->Vector2:
 	return last_point.direction_to(next_point).rotated(1.6) * offset
